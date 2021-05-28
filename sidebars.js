961d0379c757a33e10c641d9b2a8dcd5fbe4ed8d
Patrick Fuchs <Patrick Fuchs@DESKTOP-7DR07KD>
2021-05-28 23:21:57 +0200

    sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,6 @@
 module.exports = {
   someSidebar: {
     COINs Team 5: ['doc1'],
-    Knowledge: ['doc2','doc3'],
-    Project Status Group 5: ['doc4', 'doc5']
+    Knowledge: ['doc2', 'doc3'],
   },
 };
