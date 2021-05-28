6a632c4f04ae1b5c1ce2987b04f7b43643a30f49
Patrick Fuchs <Patrick Fuchs@DESKTOP-7DR07KD>
2021-05-28 23:19:14 +0200

    Menü

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,7 @@
 module.exports = {
   someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
+    COINs Team 5: ['doc1'],
+    Knowledge: ['doc2','doc3'],
+    Project Status Group 5: ['doc4', 'doc5']
   },
 };
