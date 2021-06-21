f4a03536bd8b208fab3b3cc30e163001ee4ae520
Patrick Fuchs <patrick.fuchs89@gmx.de>
2021-06-21 11:55:39 +0200

    Sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,8 @@
 module.exports = {
   someSidebar: {
     "About our project": ["doc1"],
-    Knowledge: ["doc2", "doc3"],
+    Knowledge: ["doc2"],
+    Literature: ["doc3"],
     "Project Status": ["doc4", "doc5", "doc6"],
   },
 };
