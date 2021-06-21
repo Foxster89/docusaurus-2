40b57ed6758f0db811a4a49fb770d2732707fb7e
Patrick Fuchs <patrick.fuchs89@gmx.de>
2021-06-21 11:52:47 +0200

    sidebar active

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -107,13 +107,13 @@ module.exports = {
     [
       "@docusaurus/preset-classic",
       {
-       /* docs: {
+        docs: {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           editUrl:
             "https://github.com/facebook/docusaurus/edit/master/website/",
         },
-        blog: {
+       /* blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
